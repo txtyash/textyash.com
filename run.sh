7abceb1d9bd33ea923c2ff08c3c1b0c33b37e884
evccyr <evccyr@gmail.com>
2024-02-05 14:05:44 +0530

    Start implementing blogs route.

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 # Compile source code and run
-# Also run npm for tailwind
 # -q: quiet, -c: clear screen, -w: dir to watch, -x: command
 # cargo watch -c -x r -w src/ & # Without shuttle
-npm run watch &
+npm run watch &                                     # Check package.json
 cargo watch -c -w src/ -x "shuttle run --port 8090" # With shuttle
--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,92 @@
 mod db;
 use axum::{
-    debug_handler,
     extract::State,
-    response::{Html, IntoResponse, Response},
+    response::{Html, IntoResponse},
     routing::get,
-    Router,
+    Form, Router,
 };
-use db::{
-    connection,
-    ConnectionType::{Local, Remote},
-};
-use libsql::{Connection, Database};
-use std::sync::{Arc, Mutex};
-use tera::{Context, Tera};
-use tower_http::services::{ServeDir, ServeFile};
+use db::{connection, ConnectionType::Local};
+use lazy_static::lazy_static;
+use libsql::Connection;
+use serde::Deserialize;
+use std::sync::Arc;
+use tera::Tera;
+use tower_http::services::ServeDir;
+
+lazy_static! {
+    pub static ref TEMPLATES: Tera = {
+        let tera = match Tera::new("templates/**/*") {
+            Ok(t) => t,
+            Err(e) => {
+                println!("Parsing error(s): {}", e);
+                ::std::process::exit(1);
+            }
+        };
+        tera
+    };
+}
 
 #[derive(Clone)]
 struct AppState {
-    templates: tera::Tera,
     db: Connection,
 }
 
 #[shuttle_runtime::main]
 async fn axum() -> shuttle_axum::ShuttleAxum {
-    let mut templates = Tera::new("templates/**/*").expect("Failed to initialize Tera.");
-    // templates
-    //     .full_reload()
-    //     .expect("Failed to re-render templates.");
     let db = connection(Local).await;
-    let state = Arc::new(AppState { templates, db });
+    let state = AppState { db };
 
     let routers = Router::new()
         .nest_service("/static", ServeDir::new("static"))
         .nest_service("/components", ServeDir::new("templates/components"))
         .route("/", get(root))
+        .nest("/blogs", routes_blogs())
         .with_state(state);
 
     Ok(routers.into())
 }
 
-async fn root(State(state): State<Arc<AppState>>) -> impl IntoResponse {
+fn routes_blogs() -> Router<AppState> {
+    Router::new().route("/new", get(blog_new).post(blog_post_new))
+}
+
+async fn root(State(_state): State<AppState>) -> impl IntoResponse {
     let mut ctx = tera::Context::new();
-    ctx.insert("css_tailwind", "static/css/output.css");
-    ctx.insert("script_htmx", "static/js/htmx.min.js");
+    ctx.insert("css_tailwind", "/static/css/output.css");
     ctx.insert(
         "image_profile_pic",
-        "static/images/profile-pic-placeholder.jpg",
+        "/static/images/profile-pic-placeholder.jpg",
     );
-    ctx.insert("icon_github", "static/icons/github-32.webp");
-    ctx.insert("icon_instagram", "static/icons/instagram-32.webp");
-    ctx.insert("icon_twitter", "static/icons/twitter-32.webp");
-    ctx.insert("icon_linkedin", "static/icons/linkedin-32.webp");
-    ctx.insert("icon_reddit", "static/icons/reddit-32.webp");
-    ctx.insert("icon_rss", "static/icons/rss-32.webp");
-    let template = &state.templates;
-    let db = &state.db;
+    ctx.insert("icon_github", "/static/icons/github-32.webp");
+    ctx.insert("icon_instagram", "/static/icons/instagram-32.webp");
+    ctx.insert("icon_twitter", "/static/icons/twitter-32.webp");
+    ctx.insert("icon_linkedin", "/static/icons/linkedin-32.webp");
+    ctx.insert("icon_reddit", "/static/icons/reddit-32.webp");
+    ctx.insert("icon_rss", "/static/icons/rss-32.webp");
 
     let file = "index.html";
     Html(
-        template
+        TEMPLATES
             .render(file, &ctx)
             .expect("Failed to render {file}"),
     )
+}
+
+async fn blog_new() -> impl IntoResponse {
+    let mut ctx = tera::Context::new();
+    ctx.insert("css_tailwind", "/static/css/output.css");
+    ctx.insert("css_easymde", "/static/css/easymde.min.css");
+    ctx.insert("js_easymde", "/static/js/easymde.min.js");
+
+    let file = "blogs/new/index.html";
+    Html(
+        TEMPLATES
+            .render(file, &ctx)
+            .expect("Failed to render {file}"),
+    )
+}
+
+#[derive(Debug, Deserialize)]
+struct Blog {
+    content: String,
 }
