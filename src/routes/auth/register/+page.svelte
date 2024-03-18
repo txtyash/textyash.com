2f45201414df68bc6074c91ff59c690f251cda8b
Yash Shinde <shinde27yash@gmail.com>
2024-03-19 01:37:48 +0530

    implements form validation

--- src/routes/auth/register/+page.svelte
+++ src/routes/auth/register/+page.svelte
@@ -1,20 +1,43 @@
-<script>
+<script lang="ts">
   import Icon from "@iconify/svelte";
-  let showPassword = true;
+  import {
+    checkPassword,
+    checkDisplayName,
+    checkEmail,
+    checkUsername,
+    validForm,
+  } from "../formValidator";
+  /* TODO: beforeSubmit
+  if password === confirmPassword
+  if !validForm({object}) then showAllWarnings = true;
+  */
+  let showPassword = false;
+  // TODO: trim displayName before submit
+  let displayName = "";
   let username = "";
-  $: username = username.replace(/[^0-9]/g, "");
+  $: username = username.trim();
+  let email = "";
+  $: email = email.trim();
+  let password = "";
+  $: password = password.trim();
+  let confirmPassword = "";
+  $: confirmPassword = confirmPassword.trim();
+  let showAllWarnings = false;
+  let focusedField = "";
 </script>
 
 <div class="h-screen flex flex-col items-center justify-center">
-  <h3 class="h3 my-6">Create a new account</h3>
+  <p class="text-3xl my-6">Create a new account</p>
   <form
     action="?/register"
     method="post"
     class="card h-fit py-3 px-9 rounded-xl"
   >
-    <label class="label my-4">
-      <span>Display Name</span>
+    <label class="label my-3">
+      <span><b>Display Name</b></span>
       <input
+        bind:value={displayName}
+        on:focus={() => (focusedField = "displayName")}
         class="input"
         type="text"
         name="first"
@@ -22,11 +45,15 @@
         required
       />
     </label>
+    {#if (focusedField === "displayName" || showAllWarnings) && checkDisplayName(displayName) !== ""}
+      <small>{checkDisplayName(displayName)}</small>
+    {/if}
     <!-- TODO: Allow only number, alphabet & period -->
-    <label class="label my-4">
-      <span>Username</span>
+    <label class="label my-3">
+      <span><b>Username</b></span>
       <input
         bind:value={username}
+        on:focus={() => (focusedField = "username")}
         required
         class="input"
         type="text"
@@ -34,11 +61,16 @@
         placeholder="ex: john3d"
       />
     </label>
-    <label class="label my-4">
-      <span>Email</span>
+    {#if (focusedField === "username" || showAllWarnings) && checkUsername(username) !== ""}
+      <small>{checkUsername(username)}</small>
+    {/if}
+    <label class="label my-3">
+      <span><b>Email</b></span>
       <input
         required
-        class="input my-4"
+        on:focus={() => (focusedField = "email")}
+        bind:value={email}
+        class="input my-3"
         title="Input (email)"
         type="email"
         name="email"
@@ -46,32 +78,52 @@
         autocomplete="email"
       />
     </label>
-    <label class="label my-4">
-      <span>Password</span>
-      <div class="input-group input-group-divider grid-cols-[1fr_auto]">
-        <input
-          required
-          title="Input (password)"
-          type={showPassword ? "text" : "password"}
-          name="password"
-          placeholder="Strong@Password129"
-        />
-        <button
-          type="button"
-          class="variant-filled-secondary"
-          on:click={() => (showPassword = !showPassword)}
-        >
-          <Icon icon={showPassword ? "mdi:hide" : "mdi:show"} class="h-5 w-5" />
-        </button>
-      </div>
+    {#if (focusedField === "email" || showAllWarnings) && checkEmail(email) !== ""}
+      <small>{checkEmail(email)}</small>
+    {/if}
+    <label class="label my-3">
+      <span><b>Password</b></span>
+      <input
+        required
+        on:focus={() => (focusedField = "password")}
+        bind:value={password}
+        title="Input (password)"
+        class="input my-3"
+        name="password"
+        type="password"
+        placeholder="Strong@Password129"
+      />
     </label>
-    <button type="submit" class="btn variant-filled my-4">
+    {#if (focusedField === "password" || showAllWarnings) && checkPassword(password) !== ""}
+      <small>{checkPassword(password)}</small>
+    {/if}
+    <label class="label my-3">
+      <span><b>Confirm Password</b></span>
+      <input
+        required
+        type="password"
+        class="input my-3"
+        on:focus={() => (focusedField = "confirmPassword")}
+        bind:value={confirmPassword}
+        title="Input (Confirm Password)"
+        name="confirmPassword"
+        placeholder="repeat previous password..."
+      />
+    </label>
+    {#if focusedField === "confirmPassword" && password !== confirmPassword}
+      <small>Passwords do not match.</small>
+    {/if}
+    <!-- Separator between above warning and below button-->
+    <div />
+    <button type="submit" class="btn variant-filled my-3">
       <span>
         <Icon icon="mingcute:check-fill" class="h-7 w-7" />
       </span>
       <span>Register</span>
     </button>
   </form>
-  <a class="p-3 m-3" href="/auth/login">Already have an account? <u>Login</u></a
-  >
+  <a class="p-3 m-3" href="/auth/login">
+    Already have an account?
+    <u>Login</u>
+  </a>
 </div>
