97877d363c84695f7295a35a8898a18801b99502
Yash Shinde <shinde27yash@gmail.com>
2024-04-02 01:54:10 +0530

    fixes posts ordering issue

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,6 @@
 import type { PageLoad } from "./$types";
 import { db, posts } from "$lib/server/db";
-import { sql, getTableColumns } from "drizzle-orm";
+import { desc, sql, getTableColumns } from "drizzle-orm";
 
 export const load: PageLoad = async ({ params }) => {
   const allPosts = await db
@@ -8,7 +8,8 @@ export const load: PageLoad = async ({ params }) => {
       ...getTableColumns(posts),
       createdAt: sql<string>`to_char(created_at, 'Mon DD, YYYY')`,
     })
-    .from(posts);
+    .from(posts)
+    .orderBy(desc(posts.createdAt));
 
   if (allPosts.length === 0) {
     return;
