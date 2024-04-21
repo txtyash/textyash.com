0733d4add9a0cb3dafbd2629712518370efb3d35
Yash Shinde <shinde27yash@gmail.com>
2024-04-21 14:11:25 +0530

    Improves landing page design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -35,22 +35,31 @@
 
 {#if animate}
 	<div in:fly={{ y: -200, duration: 2000 }} out:fade>
-		<div class="m-2 grid grid-cols-2 items-center p-2">
+		<div class="m-2 my-8 grid grid-cols-2 items-center p-2">
 			<img
-				class="mx-auto h-32 w-32 rounded-xl object-cover sm:h-48 sm:w-48"
+				class="mx-auto h-32 w-32 rounded-xl object-cover outline outline-offset-4 sm:h-48 sm:w-48"
 				src="/images/profile.webp"
 				alt="Yash's profile"
 			/>
-			<p class="flex items-center text-sm sm:text-lg">
-				Hi, I’m Yash, a software developer. I love technology & music. I occasionaly jot down my
-				thoughts here : )
+			<p class="text-sm sm:text-lg">
+				Hi, I’m Yash, a software developer. I <span
+					class="animate-pulse underline decoration-pink-500 decoration-wavy">love</span
+				> technology & music. I occasionaly jot down my thoughts here : )
 			</p>
 		</div>
 
-		<div class="m-2 flex w-auto justify-center px-4">
-			<button type="button" on:click={() => (showPosts = !showPosts)} class="variant-filled btn">
+		<div class="relative m-2 flex w-auto justify-center px-4">
+			<button
+				type="button"
+				on:click={() => (showPosts = !showPosts)}
+				class="variant-filled btn z-10"
+			>
 				{showPosts ? 'Show Links' : 'Show Posts'}
 			</button>
+			<!-- MY HORIZONTAL LINE -->
+			<div
+				class="absolute top-1/2 z-0 h-0.5 w-full -translate-y-1/2 bg-stone-400 dark:bg-stone-400"
+			/>
 		</div>
 	</div>
 {/if}
